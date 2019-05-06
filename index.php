ce2d3d62542fc40e95b48741cc04145da3ee4977
Stefania <stefania@grandhood.dk>
2019-05-06 11:31:51 +0200

    Pushing navigation

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php 
   require_once 'top.php';
 ?>
-        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-                <div class="container">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                </div>
-              </nav>
         <input type="text" name="datetimes" />
 
         <input type="checkbox" checked data-toggle="toggle">
