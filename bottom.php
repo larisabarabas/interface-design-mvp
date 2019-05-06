4a464b1bea9ece0b14dd683d455f36f7e0c5db04
Stefania <stefania@grandhood.dk>
2019-05-06 11:15:57 +0200

    Adding php

--- bottom.php
+++ bottom.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-    <title>Rento</title>
-</head>
-<body>
-        <input type="text" name="datetimes" />
-
-        <input type="checkbox" checked data-toggle="toggle">
-
-
-    
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+  require_once 'top.php';
+?>
+        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+                <div class="container">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                </div>
+              </nav>
+        <input type="text" name="datetimes" />
+
+        <input type="checkbox" checked data-toggle="toggle">
+
+<?php 
+  require_once 'bottom.php';
+?>
+    
+
