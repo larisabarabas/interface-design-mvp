374a774d4062fd4df3b9fa72052c08b755bc76d1
Ioana <ioanav3796@gmail.com>
2019-05-09 13:27:16 +0200

    styling almost done for manage-booking.php

--- manage-booking.php
+++ manage-booking.php
@@ -3,31 +3,47 @@
   require_once 'top.php';
 ?>
 
-<h1>Manage Booking</h1>
+<div class="main-container">
+    <h1 class="margin-bottom-1rem">Manage Booking</h1>
     <div class="card" >
         <img class="card-img-top" src="./images/2015-citroen-c1-airscape-feel-3door-microcar-black.png" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title text-center">Mini Manual</h5>
-            <p class="card-text car-info">Booking reference: <span>GFR67L</span></p>
+            <h1 class="text-center card-title">Mini manual</h1>
+            <p class="card-text car-info">Booking reference: <span id="booking-reference">GFR67L</span></p>
         </div>
     </div>
     <div class="card info-link">
-        <h3>Pickup</h3>
+        <h1 class="margin-bottom-1rem">Pickup</h1>
         <p>Copenhagen Airport, CPH</p>
         <p class="ui-widget-header">Fri, May 21, 10:00 AM</p>
     </div>
     <div class="card info-link">
-        <h3>Return</h3>
+        <h1 class="margin-bottom-1rem">Return</h1>
         <p>Copenhagen Airport, CPH</p>
         <p class="ui-widget-header">Fri, May 21, 10:00 AM</p>
     </div>
-    <div class="card info-link">
-        <h3>Selected equipment</h3>
-        <div>
-            <img src="" alt="">
-            <span>Baby seat</span>
+
+
+    <h1 class="margin-bottom-1rem">Car details</h1>
+        <div class="row padding-bottom-20">
+            <div class="col text-left"><img class="icon" src="images/car-door.svg"/>5 doors</div>
+            <div class="col text-left"><img class="icon" src="images/person.svg"/>4 seats</div>
+        </div>
+        <div class="row">
+            <div class="col text-left"><img class="icon" src="images/gears.svg"/>Manual</div>
+            <div class="col text-left"><img class="icon" src="images/ac.svg"/>A/C</div>
         </div>
     </div>
+
+
+    <div class="card info-link container text-center">
+        <h1 class="margin-bottom-1rem">Selected equipment</h1>
+        <div class="row">
+            <div class="col"><img class="icon" src="images/baby.svg"/>Baby seat</div>
+        </div>
+    </div>
+
+</div>
 
 <?php 
     require_once 'bottom.php';
