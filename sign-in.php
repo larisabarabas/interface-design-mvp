02e7e59ef60929c4b295081ca1adb5fb34c9e0bf
Stefania <stefania@grandhood.dk>
2019-05-13 11:23:36 +0200

    pushing functionality for signup and signin

--- sign-in.php
+++ sign-in.php
@@ -5,9 +5,9 @@
 
     <div class="main-container">
         <h1>Sign in</h1>
-        <input type="text" placeholder="Email">
-        <input type="password" placeholder="Password">
-        <a href="search.php" class="green-btn">Sign in</a>
+        <input type="email" placeholder="Email" id="loginEmail">
+        <input type="password" placeholder="Password" id="loginPassword">
+        <a class="green-btn" onclick='login()'>Sign in</a>
         <div class="bottom-links">
             <a href="#">Forgot password?</a>
             <a href="sign-up.php">Sign up</a>
@@ -16,6 +16,7 @@
 
     
 <?php 
+    $sLinkToScript = '<script src="js/signin.js"></script>';
     require_once 'bottom.php';
 ?>
 
