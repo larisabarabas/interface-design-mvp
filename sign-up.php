1b741460c1e31133c345716d658fd6824bdcc44c
Marija <mari4w04@stud.kea.dk>
2019-05-13 11:35:27 +0200

    Small changes

--- sign-up.php
+++ sign-up.php
@@ -9,7 +9,7 @@
         <input type="text" placeholder="Last name">
         <input type="text" id="date_ex" name="dob" placeholder="Birth date">
         <input type="text" placeholder="Email">
-        <input type="text" placeholder="Password">
+        <input type="password" placeholder="Password">
 
         <a href="search.php" class="green-btn">Sign up</a>
 
