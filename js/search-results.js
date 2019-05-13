afa92f51eb30d127446ac4b303589ff4234026aa
Ioana <ioanav3796@gmail.com>
2019-05-13 13:54:35 +0200

    view-booking

--- js/search-results.js
+++ js/search-results.js
@@ -9,14 +9,14 @@ let btns = [btnBookCar1, btnBookCar2, btnBookCar3];
 let carName = btnBookCar1.parentElement.childNodes[0].nextElementSibling.innerText;
 let carImgPath = btnBookCar1.parentElement.parentElement.childNodes[0].nextElementSibling.src;
 
-btns.forEach(function(btn){
-    btn.addEventListener("click", ()=>{
+btns.forEach(function (btn) {
+    btn.addEventListener("click", () => {
         let carName = btn.parentElement.childNodes[0].nextElementSibling.innerText;
         let carImgPath = btn.parentElement.parentElement.childNodes[0].nextElementSibling.src;
-    
+
         localStorage.setItem('searchResultsCarName', carName);
         localStorage.setItem('searchResultsCarImgPath', carImgPath);
 
-        window.location.href = 'confirmation.php';
+        window.location.href = 'car-details.php';
     });
 });
